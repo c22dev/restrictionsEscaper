b9d1cc9a91059851309869eb85dfcafd1bf5aab7
c22dev <102235607+c22dev@users.noreply.github.com>
2024-03-12 17:33:42 +0100

    fix

--- installer.sh
+++ installer.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Constantin CLERC - v0.1 - 12.03.2024
 
-cd "$HOME/Library/LaunchAgents/"
-curl -L https://raw.githubusercontent.com/c22dev/restrictionsEscaper/main/ch.cclerc.proxystart.plist > ch.cclerc.proxystart.plist
-awk -v home="$HOME" '{gsub("/Users/etudiant", home)} 1' "$HOME/Library/LaunchAgents/ch.cclerc.proxystart.plist" > tmpfile && mv tmpfile "$HOME/Library/LaunchAgents/ch.cclerc.proxystart.plist"
+if [ ! -e "$HOME/.restrEscape" ]; then
+  cd "$HOME/Library/LaunchAgents/"
+  curl -L https://raw.githubusercontent.com/c22dev/restrictionsEscaper/main/ch.cclerc.proxystart.plist > ch.cclerc.proxystart.plist
+  awk -v home="$HOME" '{gsub("/Users/etudiant", home)} 1' "$HOME/Library/LaunchAgents/ch.cclerc.proxystart.plist" > tmpfile && mv tmpfile "$HOME/Library/LaunchAgents/ch.cclerc.proxystart.plist"
+  echo "Installed daemon launcher"
+fi
 # First of all, let's prepare.
 cd $HOME
 mkdir .restrEscape
